603b941052a414cad2e851fcf1d690f5f1529674
David Stewart <david.j.stewart@yale.edu>
2021-01-28 11:03:20 -0500

    addendum

--- src_io/io_fnc.cc
+++ src_io/io_fnc.cc
@@ -300,6 +300,13 @@ string io_geant05_ascii(int geantid) {
         case 12: return "antiK";
         case 14: return "P";
         case 15: return "pbar";
+
+        case 0: return "pi";
+        case 1: return "antipi";
+        case 2: return "K";
+        case 3: return "antiK";
+        case 4: return "P";
+        case 5: return "pbar";
     }
     return "none";
 };
@@ -311,6 +318,13 @@ string io_geant05_greek(int geantid) {
         case 12: return "K^{-}";
         case 14: return "p";
         case 15: return "#bar{p}";
+
+        case 0: return "#pi";
+        case 1: return "#pi^{-}";
+        case 2: return "K";
+        case 3: return "K^{-}";
+        case 4: return "p";
+        case 5: return "#bar{p}";
     }
     return "none";
 };
