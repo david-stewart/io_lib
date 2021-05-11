7540d403716fb6c8504bde08e94edf6ffc643762
David Stewart <david.j.stewart@yale.edu>
2021-05-11 06:43:19 -0400

    Update for some some tags and binning spectific to p+Au 2015 data

--- pAu2015_common/src/ioBins.h
+++ pAu2015_common/src/ioBins.h
@@ -5,4 +5,12 @@
 double* ioBins_jetpt_prelim_13bins(); // 14 edges for 13 bins
 double* ioBins_BBCES_3bin(); // 3 bins, four edges
 double* ioBins_BBCES_4bin(); // 3 bins, four edges
+
+struct ioBins_BBCES_3bins_binner {
+    ioBins_BBCES_3bins_binner ();
+    int last_val;
+    int operator();
+    int operator(double ea); // convert EA to which bin
+}
+
 #endif
