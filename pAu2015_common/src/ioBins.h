288a76641351ec0a9f56404f198e493e41bdc166
David Stewart <david.j.stewart@yale.edu>
2021-05-11 07:42:47 -0400

    Small update

--- pAu2015_common/src/ioBins.h
+++ pAu2015_common/src/ioBins.h
@@ -2,7 +2,7 @@
 #define ioBins__h
 
 // for binning edges
-double* ioBins_jetpt_prelim_13bins(); // 14 edges for 13 bins
+double* ioBins_prelim_jetpt_13bins(); // 14 edges for 13 bins
 double* ioBins_BBCES_3bin(); // 3 bins, four edges
 double* ioBins_BBCES_4bin(); // 3 bins, four edges
 
