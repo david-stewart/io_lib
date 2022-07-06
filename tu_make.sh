2684ed95643e1b26b5712f2b26e7a5f0b7ab8fea
David Stewart <ds.johnny@gmail.com>
2022-07-06 08:45:27 -0400

    update

---
+++ tu_make.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# set up files
+for file in include lib obj; do
+if [[ ! -d ${file} ]]; then
+    mkdir ${file}
+fi
+done
+
+# ------------------------------
+# working on src files
+# ------------------------------
+
+# link the header files
+cd include
+while read line; do
+    if [[ $line = \#* ]] ; then continue; fi
+    # echo $line
+    cline=${line%$'\r'}
+    if [ ! -L tu${cline}.h ]; then
+        ln -s ../src/tu${cline}.h .
+    fi
+done < ../tu_lib_list
+cd ..
+
+# compile files
+temp_script=root_L_script.sh
+echo 'root -l <<EOF' > ${temp_script}
+# echo 'gSystem->Load("${ROOUNFOLD}/libRooUnfold.so");' >> ${temp_script}
+while read line; do
+    if [[ $line = \#* ]] ; then continue; fi
+    cline=${line%$'\r'}
+    echo "cout << \".L src/tu${cline}.cc+\" << endl;" >> ${temp_script}
+    echo ".L src/tu${cline}.cc+" >> ${temp_script}
+done < tu_lib_list
+echo 'EOF' >> ${temp_script}
+chmod u+x ${temp_script}
+./${temp_script}
+
+cd lib
+while read line; do
+    if [[ $line = \#* ]] ; then continue; fi
+    cline=${line%$'\r'}
+    if [ ! -L libtu${cline}.so ]; then
+        ln -s ../src/tu${cline}_cc.so libtu${cline}.so
+    fi
+    if [ ! -L tu${cline}_cc_ACLiC_dict_rdict.pcm ]; then
+        ln -s ../src/tu${cline}_cc_ACLiC_dict_rdict.pcm
+    fi
+done < ../tu_lib_list
+cd ..
